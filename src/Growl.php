6c92c2b4b901bb87c2435d2fabe5473f02ee8aea
Mohammad Nadzif <demihuman@live.com>
2018-09-05 12:58:16 +0700

    class config

--- src/Growl.php
+++ src/Growl.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace nadzif\growl;
 
 use rmrevin\yii\fontawesome\FAS;
@@ -17,30 +18,26 @@ class Growl extends Widget
     const COLOR_SOLID   = 'alert-solid';
 
     public $icon;
-    public $title      = 'title';
-    public $message    = 'message';
-    public $options    = [];
-    public $background = 'alert-success';
-    public $time       = 3000;
+    public $containerOptions = ['class' => 'd-sm-flex align-items-center justify-content-start alert-content'];
+    public $title            = 'title';
+    public $message          = 'message';
+    public $options          = [];
+    public $background       = 'alert-success';
+    public $time             = 3000;
 
     public function init()
     {
         parent::init();
-
-        if ($this->icon === null) {
-            $this->icon = FAS::icon(FontAwesome::_BELL);
-        }
-
         $this->options = ['options' => $this->options];
     }
 
     public function run()
     {
         Alert::begin($this->options);
-        echo Html::beginTag('div', ['class' => 'd-sm-flex align-items-center justify-content-start alert-content']);
-        echo Html::beginTag('div', ['class' => 'alert-icon']);
-        echo $this->icon;
-        echo Html::endTag('div');
+        echo Html::beginTag('div', $this->containerOptions);
+        if ($this->icon) {
+            echo Html::tag('div', $this->icon, ['class' => 'alert-icon']);
+        }
         echo Html::beginTag('div', ['class' => 'mg-sm-l-15 mg-t-15 mg-sm-t-0']);
         echo Html::tag('h5', $this->title, ['class' => 'mg-b-2 pd-t-2']);
         echo Html::tag('p', $this->message, ['class' => 'mg-b-0 tx-xs op-8']);
